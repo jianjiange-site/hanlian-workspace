bc4492134087452923c782675b75ef3eb9a45ce9
hanlian <pengy9883@gmail.com>
2026-06-12 23:19:14 +0800

    chore: bootstrap dating-server modules

---
+++ dating-server/common/src/main/java/com/jianjiange/dating/common/CommonModule.java
@@ -0,0 +1,11 @@
+package com.jianjiange.dating.common;
+
+public final class CommonModule {
+
+    private CommonModule() {
+    }
+
+    public static String name() {
+        return "common";
+    }
+}
---
+++ dating-server/im-service/src/main/java/com/jianjiange/dating/im/PingController.java
@@ -0,0 +1,17 @@
+package com.jianjiange.dating.im;
+
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/internal/ping")
+    public Map<String, String> ping() {
+        return Map.of(
+                "service", "im-service",
+                "status", "ok"
+        );
+    }
+}
